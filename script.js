788d71f6f374b544d82bb3b427b36cce90f8b38e
minyamir <minyamirkelemu12@gmail.com>
2025-02-21 13:18:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openModal(src) {
+    document.getElementById("modalImage").src = src;
+    document.getElementById("myModal").style.display = "block";
+}
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
